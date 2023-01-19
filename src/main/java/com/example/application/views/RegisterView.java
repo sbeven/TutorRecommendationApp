481c4fb7582fe8e83d438a56b835779a8a879de4
Steven Zhang <50186662+sbeven@users.noreply.github.com>
2023-01-18 20:32:53 -0500

    tutor shows up on list whne signed in- still need to fix edge cases

--- src/main/java/com/example/application/views/RegisterView.java
+++ src/main/java/com/example/application/views/RegisterView.java
@@ -1,6 +1,10 @@
 package com.example.application.views;
 
+import com.example.application.data.entity.Contact;
+import com.example.application.data.entity.Status;
+import com.example.application.data.service.CrmService;
 import com.example.application.security.SecurityConfig;
+import com.example.application.views.list.ContactForm;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -24,19 +28,27 @@ import java.util.TreeMap;
 @PageTitle("Register | Sample App")
 @AnonymousAllowed
 public class RegisterView extends VerticalLayout {
+	private TextField first = new TextField("First Name");
+	private TextField last = new TextField("Last Name");
 	private TextField username = new TextField("Username");
 	private TextField password = new TextField("Password");
+	private EmailField email = new EmailField("Email");
 	private ComboBox<String> roles = new ComboBox<>("Sign up as");
+	private ComboBox<Status> subject = new ComboBox<>("Subject (if tutor)");
 	private Button signup = new Button("Sign up");
 	private Button back = new Button("Cancel");
-
-
-	public RegisterView() {
+	private CrmService service;
+	public static HashMap<String, String> userAndRoles = new HashMap<String, String>();
+	public RegisterView(CrmService service) {
+		this.service = service;
+		subject.setItems(service.findAllStatuses());
+		subject.setItemLabelGenerator(Status::getName);
 		roles.setItems(new String[]{"Student", "Tutor"});
 		signup.addClickListener(
 				e -> signup.getUI().ifPresent(ui -> signUp(ui)));
 		back.addClickListener(e -> back.getUI().ifPresent(ui -> ui.navigate("")));
-		FormLayout form = new FormLayout(username, password, roles, signup, back);
+		FormLayout form = new FormLayout(first, last, username, password, email, roles, subject, signup, back);
+		form.setColspan(email, 2);
 		add(form);
 		setJustifyContentMode(JustifyContentMode.CENTER);
 	}
@@ -45,12 +57,23 @@ public class RegisterView extends VerticalLayout {
 		String user = username.getValue();
 		String pass = password.getValue();
 		String role = roles.getValue();
+		String f = first.getValue();
+		String l = last.getValue();
+		String e = email.getValue();
+		Status s = subject.getValue();
 
 		if(pass.trim().isEmpty()){
 			Notification.show("You must fill in all three fields");
 			navigation.navigate("register");
 		} else {
 			SecurityConfig.addUser(user, pass, role);
+			Contact c = new Contact();
+			c.setFirstName(f);
+			c.setLastName(l);
+			c.setEmail(e);
+			c.setStatus(s);
+			service.saveContact(c);
+			userAndRoles.put(user, role);
 			navigation.navigate("login");
 		}
 	}
